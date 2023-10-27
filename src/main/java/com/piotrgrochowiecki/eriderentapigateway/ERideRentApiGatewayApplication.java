f0f1ad39144a312e416629c493e716ae9b17e2ba
Piotr Grochowiecki <Piotr.Grochowiecki@dxc.com>
2023-10-27 19:25:47 +0200

    Initial commit

---
+++ src/main/java/com/piotrgrochowiecki/eriderentapigateway/ERideRentApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.piotrgrochowiecki.eriderentapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERideRentApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ERideRentApiGatewayApplication.class, args);
+    }
+
+}
