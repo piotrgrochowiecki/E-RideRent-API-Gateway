601d745a56761acec734ec1bf813895a9162a4d0
piotrgrochowiecki <piotrgrochowiecki@gmail.com>
2023-10-30 17:19:52 +0100

    added env variables and AppConfig

--- src/main/java/com/piotrgrochowiecki/eriderentapigateway/ERideRentApiGatewayApplication.java
+++ src/main/java/com/piotrgrochowiecki/eriderentapigateway/ERideRentApiGatewayApplication.java
@@ -2,20 +2,12 @@ package com.piotrgrochowiecki.eriderentapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class ERideRentApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ERideRentApiGatewayApplication.class, args);
-    }
-
-    @Bean
-    public WebClient webClient() {
-        return WebClient.builder()
-                .build();
     }
 
 }
