12a95fe9d62c0be39a3751775c1caae47b02abec
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2023-11-05 14:43:05 +0100

    added header based authentication (#3)

--- src/main/java/com/piotrgrochowiecki/eriderentapigateway/remote/client/AuthenticationServiceClient.java
+++ src/main/java/com/piotrgrochowiecki/eriderentapigateway/remote/client/AuthenticationServiceClient.java
@@ -1,10 +1,9 @@
 package com.piotrgrochowiecki.eriderentapigateway.remote.client;
 
-import com.piotrgrochowiecki.eriderentapigateway.remote.dto.AuthenticationRequestDto;
 import org.springframework.http.ResponseEntity;
 
 public interface AuthenticationServiceClient {
 
-    ResponseEntity<?> authenticate(AuthenticationRequestDto requestDto);
+    ResponseEntity<?> authenticate(String authenticationHeader);
 
 }
