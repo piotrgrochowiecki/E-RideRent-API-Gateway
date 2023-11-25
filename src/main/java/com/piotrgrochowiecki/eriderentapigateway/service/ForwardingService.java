1e7321120b85431c2fdccc813ab6367b9dc5ee65
piotrgrochowiecki <piotrgrochowiecki@gmail.com>
2023-11-25 16:12:21 +0100

    returned new response entity object in forward method of ForwardingService

--- src/main/java/com/piotrgrochowiecki/eriderentapigateway/service/ForwardingService.java
+++ src/main/java/com/piotrgrochowiecki/eriderentapigateway/service/ForwardingService.java
@@ -20,22 +20,26 @@ public class ForwardingService {
 
     public ResponseEntity<?> forward(HttpServletRequest request, String body) {
         String endpoint = buildEndpoint(request.getRequestURI());
-        return webClient.post()
+        ResponseEntity<?> responseEntityFromService = webClient.post()
                     .uri(endpoint)
                     .contentType(MediaType.APPLICATION_JSON)
                     .accept(MediaType.APPLICATION_JSON)
                     .body(BodyInserters.fromValue(body))
                     .exchangeToMono(clientResponse -> clientResponse.toEntity(String.class))
                     .block();
-        }
+
+        return transformResponseEntity(responseEntityFromService);
+    }
 
     public ResponseEntity<?> forward(HttpServletRequest request) {
         String endpoint = buildEndpoint(request.getRequestURI());
-        return webClient.get()
+        ResponseEntity<?> responseEntityFromService = webClient.get()
                     .uri(endpoint)
                     .accept(MediaType.APPLICATION_JSON)
                     .exchangeToMono(clientResponse -> clientResponse.toEntity(String.class))
                     .block();
+
+        return transformResponseEntity(responseEntityFromService);
     }
 
     private String buildEndpoint(String uri) {
@@ -46,6 +50,16 @@ public class ForwardingService {
             }
         }
         throw new BadRequestRuntimeException("Could not build correct URL");
+    }
+
+    private ResponseEntity<?> transformResponseEntity(ResponseEntity<?> responseEntityFromService) {
+        if (responseEntityFromService != null) {
+            return ResponseEntity.status(responseEntityFromService.getStatusCode())
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(responseEntityFromService.getBody());
+        } else {
+            throw new RuntimeException("Did not receive response from server");
+        }
     }
 
 }
